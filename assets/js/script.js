95eea77bdfab7f3bb195d2ddab8e4bfbfadcf44f
David Ward <davidward1307@gmail.com>
2022-03-14 17:52:44 +0000

    added score
    added score functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector("[data-computer-score]")
+const yourScoreSpan = document.querySelector("[data-your-score]")
 const SELECTIONS = [
     {
         name: "rock",
@@ -15,19 +18,42 @@ const SELECTIONS = [
 ]
 
 
-selectionButtons.forEach(selectionButtons => {
-    selectionButtons.addEventListener('click', e => {
-        const selectionName = selectionButtons.dataset.selection
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
         const selection = SELECTIONS.find(selection => selection.name === selectionName)
         makeSelection(selection)
     })
 })
 
 function makeSelection(selection){
-    const computerSelection
+    const computerSelection = randomSelection();
+    const yourWinner = isWinner(selection, computerSelection);
+    const oppWinner = isWinner(computerSelection, selection);
+
+    addSelectionResult(computerSelection, oppWinner);
+    addSelectionResult(selection, yourWinner);
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (oppWinner)incrementScore(computerScoreSpan)
 }
 
-function isWinner
+function incrementScore(scoreSpan)
+{
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function addSelectionResult(selection, winner){
+    const div = document.createElement("div")
+    div.innerText = selection.name
+    div.classList.add("result-selection")
+    if(winner) div.classList.add("winner")
+    finalColumn.after(div)
+    
+}
+
+function isWinner(selection, opponentSelection){
+    return selection.beats === opponentSelection.name
+}
 
 function randomSelection(){
     const randomInt = Math.floor(Math.random() * SELECTIONS.length);
